d05d72d2b795f2a49710481abf8412fe14938e1a
Stefan <stefan.kraus@posteo.eu>
2020-04-20 21:47:59 +0200

    committed

--- src/Homework/HWExercise4/DuplicateValuesOfString.java
+++ src/Homework/HWExercise4/DuplicateValuesOfString.java
@@ -1,24 +1,32 @@
 package Homework.HWExercise4;
 
-import java.lang.reflect.*;
+
 import java.util.*;
 
 public class DuplicateValuesOfString {
     public static String returnDuplicateCharacters(){
         HashSet<Character> duplicateCharacters = new HashSet<>();
+        char[] duplicates = new char[0];
         String stringWithDuplicateCharacters = "ReDi School is cool";
         String lowerCaseString = stringWithDuplicateCharacters.toLowerCase();
 
 
-        for (int i = 0; i<=stringWithDuplicateCharacters.length()-1;i++){
+        for (int i = 0; i<stringWithDuplicateCharacters.length();i++) {
             //String lowerCaseString = stringWithDuplicateCharacters.toLowerCase();
             //System.out.println(lowerCaseString);
-            lowerCaseString = lowerCaseString + stringWithDuplicateCharacters.charAt(i);
+            lowerCaseString = " " + stringWithDuplicateCharacters.charAt(i);
+            for (int j = i + 1; j < stringWithDuplicateCharacters.length(); j++) {
+                if (stringWithDuplicateCharacters.charAt(i) == stringWithDuplicateCharacters.charAt(j)) {
+                    duplicates[i] = duplicates[j];
+                }
+                System.out.println(lowerCaseString);
+            }
         }
-        System.out.println(lowerCaseString);
+        //System.out.println(lowerCaseString);
 
         return lowerCaseString;
     }
+
 
     public static void main(String[] args) {
 
